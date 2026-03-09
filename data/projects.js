6439f3b2290ce9f45a385db14703deedff0ce7a6
Jproda <145200961+Joose15@users.noreply.github.com>
2026-03-09 12:43:02 +0100

    Add files via upload

---
+++ data/projects.js
@@ -0,0 +1,18 @@
+const projects = [
+  {
+    name: "Energy Monitoring",
+    file: "projects/energy.html"
+  },
+  {
+    name: "IoT Platform",
+    file: "projects/iot.html"
+  },
+  {
+    name: "Industrial Protocols",
+    file: "projects/plc.html"
+  },
+  {
+    name: "Ejemplo",
+    file: "projects/eje.html"
+  }
+]
